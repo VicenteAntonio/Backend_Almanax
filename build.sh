1b5fb6787ef886e3c04271bb9be6cf6481764f26
VicenteAntonio <vicente.antonio@uc.cl>
2025-08-21 09:26:07 -0400

    config: Updates Maven build and configuration scripts, adds GitHub credentials, and creates a script to update Maven. A new package-lock.json file has been included, and some dependencies in pom.xml have been temporarily commented out for local testing.

--- build.sh
+++ build.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+# Configura tus credenciales de GitHub
+
+# Instala utilidades necesarias
 sudo apt-get update
-sudo apt-get install dos2unix
+sudo apt-get install -y dos2unix
 dos2unix ./mvnw
+
+# Ejecuta Maven
+mvn clean install
+
+# Para ejecutar localmente el directorio Backend_Almanax
+# 1.- Verificar versión de Java (17):
+java --version
+
+# 2.- Levantar la base de datos
+
+# 3.- Dar permisos de ejecución al wrapper de Maven
+
+# 4.- Ejecutar el wrapper de Maven en modo dev
+
+./mvnw compile quarkus:dev  # o mvn compile quarkus:dev
